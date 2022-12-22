cee6aae019099627e7a58b75311c66b0682e5ddb
IvanVernichenkoDevPro <Ivan.Vernichenko@dev.pro>
2022-12-22 15:34:55 +0100

    sample rebuilt

--- sample/run.sh
+++ sample/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export DB_PASSWD='my$up3rs3cr3tp@$$word!'
-export AWS_ID='ASIIAMFAKENOPZLX6J5L'
-export AWS_KEY='w0pE4j5k4FlUrkIIAMFAKEdiLMKLGZlxyct+GpTam'
-export HOST='127.0.0.1'
-export PORT='5555'
+HOST='127.0.0.1'
+PORT='5555'
 
-docker build . -t myserver:latest
-docker run -it --rm \
-		-p 80:$PORT \
+docker build . -t myserver:latest --build-arg SECRET_ARG_1="-----Very_secret_arg_value-----" --build-arg SECRET_ARG_2="=====Also-very-secret-one======"
+
+docker run -it -d --rm \
+                -p 80:$PORT \
+                -e DB_PASSWD='my$up3rs3cr3tp@$$word!' \
+                -e AWS_ID='ASIIAMFAKENOPZLX6J5L' \
+                -e AWS_KEY='w0pE4j5k4FlUrkIIAMFAKEdiLMKLGZlxyct+GpTam' \
+                -e HOST=$HOST \
+                -e PORT=$PORT \
 		--name myserver_latest \
 		myserver:latest 
 
