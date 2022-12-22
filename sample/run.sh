1b68bebc3f8086e229e95ffffb4c082816b3c9a6
IvanVernichenkoDevPro <Ivan.Vernichenko@dev.pro>
2022-12-22 14:51:11 +0100

    env vars updated

--- sample/run.sh
+++ sample/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+export DB_PASSWD='my$up3rs3cr3tp@$$word!'
+export AWS_ID='ASIIAMFAKENOPZLX6J5L'
+export AWS_KEY='w0pE4j5k4FlUrkIIAMFAKEdiLMKLGZlxyct+GpTam'
+export HOST='127.0.0.1'
+export PORT='5555'
+
 docker build . -t myserver:latest
 docker run -it --rm \
-		-e DB_PASSWD='my$up3rs3cr3tp@$$word!' \
-		-e AWS_ID='ASIIAMFAKENOPZLX6J5L' \
-		-e AWS_KEY='w0pE4j5k4FlUrkIIAMFAKEdiLMKLGZlxyct+GpTam' \
-		-e HOST='127.0.0.1' \
-		-e PORT='5555' \
-		-p 80:5555 \
+		-p 80:$PORT \
 		--name myserver_latest \
 		myserver:latest 
 
